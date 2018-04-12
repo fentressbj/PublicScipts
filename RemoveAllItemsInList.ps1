f3beaf08718e903a1c15a38d58de7947fec1b918
fentressbj <37122512+fentressbj@users.noreply.github.com>
2018-04-12 13:50:17 -0400

    1st Commit
    Added two scripts related to finding duplicates in lists I have used multiple times

---
+++ RemoveAllItemsInList.ps1
@@ -0,0 +1,20 @@
+﻿Add-PSSnapIn Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue | Out-Null
+ 
+$web = Get-SPWeb "<add your SharePoint url here>"
+$list = $web.Lists["<Add your SharePoint List name here"]
+$caml=""
+#optional filter
+#<Where><Eq><FieldRef Name=""ContentType"" /><Value Type=""Text"">Form</Value></Eq></Where>"
+ 
+$query=new-object Microsoft.SharePoint.SPQuery
+$query.ViewAttributes = "Scope='Recursive'"
+$query.Query=$caml
+ 
+$items=$list.GetItems($query)
+Write-Host $items.Count
+ 
+$items | % { $list.GetItemById($_.Id).Delete() }
+ 
+$web.Dispose()
+$site.Dispose()
+
---
+++ RemoveDuplicatesin List.ps1
@@ -0,0 +1,14 @@
+﻿Add-PSSnapin microsoft.sharepoint.powershell 
+$web = Get-SPWeb -Identity "<insert SharePoint url here>" 
+$list = $web.Lists["<insert SharePoint list name here"]
+
+$AllDuplicates = $list.Items.GetDataTable() | Group-Object PrimaryKey ##this is the name of the column you want to compare
+ | where {$_.count -gt 1} 
+$count = 1 
+$max = $AllDuplicates.Count 
+foreach($duplicate in $AllDuplicates) 
+{ 
+$duplicate.group | Select-Object -Skip 1 | % {$list.GetItemById($_.ID).Delete()} 
+Write-Progress -PercentComplete ($count / $max * 100) -Activity "$count duplicates removed" -Status "In Progress" 
+$count++ 
+} 
